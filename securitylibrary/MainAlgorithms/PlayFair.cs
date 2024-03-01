7100b3e5ed925fa0a1f910ced066f825675bf8ae
Yahya Alnaqqep <104467283+yahyanaqqep@users.noreply.github.com>
2024-03-01 19:57:19 +0200

    Playfair Encrypt / Decrypt

--- securitylibrary/MainAlgorithms/PlayFair.cs
+++ securitylibrary/MainAlgorithms/PlayFair.cs
@@ -10,7 +10,121 @@ namespace SecurityLibrary
     {
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            key = key.ToLower();
+            string plain = "";
+            string[,] keymat = constructKeyMatrix(key);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    System.Console.WriteLine(keymat[i, j]);
+                }
+
+            }
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                System.Console.WriteLine(cipherText[i].ToString());
+                System.Console.WriteLine(itemIndex(keymat, cipherText[i].ToString())[0, 0]);
+                System.Console.WriteLine(itemIndex(keymat, cipherText[i].ToString())[0, 1]);
+                int[,] ind1 = itemIndex(keymat, cipherText[i].ToString());
+                int[,] ind2 = itemIndex(keymat, cipherText[i + 1].ToString());
+                if (ind1[0, 0] == ind2[0, 0])
+                {
+                    int[,] newletterind1 = { { ind1[0, 0], (ind1[0, 1] - 1)<0?4: (ind1[0, 1] - 1) } };
+                    int[,] newletterind2 = { { ind2[0, 0], (ind2[0, 1] - 1)<0?4: (ind2[0, 1] - 1) } };
+
+                    if (keymat[newletterind1[0, 0], newletterind1[0, 1]] == "i/j")
+                    {
+                        plain += "i";
+                    }
+                    else
+                    {
+                        plain += keymat[newletterind1[0, 0], newletterind1[0, 1]];
+                    }
+                    if (keymat[newletterind2[0, 0], newletterind2[0, 1]] == "i/j")
+                    {
+                        plain += "i";
+                    }
+                    else
+                    {
+                        plain += keymat[newletterind2[0, 0], newletterind2[0, 1]];
+                    }
+
+                }
+                else if (ind1[0, 1] == ind2[0, 1])
+                {
+                    int[,] newletterind1 = { { (ind1[0, 0] - 1)<0?4: (ind1[0, 0] - 1), ind1[0, 1] } };
+                    int[,] newletterind2 = { { (ind2[0, 0] - 1)<0?4: (ind2[0, 0] - 1), ind2[0, 1] } };
+
+                    if (keymat[newletterind1[0, 0], newletterind1[0, 1]] == "i/j")
+                    {
+                        plain += "i";
+                    }
+                    else
+                    {
+                        plain += keymat[newletterind1[0, 0], newletterind1[0, 1]];
+                    }
+                    if (keymat[newletterind2[0, 0], newletterind2[0, 1]] == "i/j")
+                    {
+                        plain += "i";
+                    }
+                    else
+                    {
+                        plain += keymat[newletterind2[0, 0], newletterind2[0, 1]];
+                    }
+                }
+                else
+                {
+                    int[,] newletterind1 = { { ind1[0, 0], ind2[0, 1] } };
+                    int[,] newletterind2 = { { ind2[0, 0], ind1[0, 1] } };
+                    if (keymat[newletterind1[0, 0], newletterind1[0, 1]] == "i/j")
+                    {
+                        plain += "i";
+                    }
+                    else
+                    {
+                        plain += keymat[newletterind1[0, 0], newletterind1[0, 1]];
+                    }
+                    if (keymat[newletterind2[0, 0], newletterind2[0, 1]] == "i/j")
+                    {
+                        plain += "i";
+                    }
+                    else
+                    {
+                        plain += keymat[newletterind2[0, 0], newletterind2[0, 1]];
+                    }
+                }
+                i++;
+            }
+            if (plain.Contains("x"))
+            {
+                for (int i = 0; i < plain.Length; i++)
+                {
+                    while (plain[i] != 'x' && i < plain.Length - 1)
+                    {
+                        i++;
+                    }
+                    if (i == plain.Length - 1)
+                    {
+                        if (plain[i] == 'x')
+                        {
+                            plain = plain.Remove(i, 1);
+                        }
+                    }
+                    else if (plain[i - 1] == plain[i + 1] )
+                    {
+                        if(plain[i - 1] == 'l' && plain[i + 1] == 'l')
+                        {
+                            continue;
+                        }
+                        plain = plain.Remove(i, 1);
+                    }
+                }
+            }
+
+            System.Console.WriteLine(plain);
+            return plain;
         }
 
         public string Encrypt(string plainText, string key)
@@ -18,72 +132,9 @@ namespace SecurityLibrary
             plainText = plainText.ToLower();
             key = key.ToLower();
             string cipher = "";
-            string[,] keymat = new string[5, 5];
-            int keyind = 0;
-            string currletter = "a";
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    if (keyind < key.Length)
-                    {
-                        if ((key[keyind] == 'i'|| key[keyind] == 'j') && !contains(keymat, "i/j"))
-                        {
-                            keymat[i, j] = "i/j";
-                        }
-                        else if(key[keyind] == 'i'&& contains(keymat, "i/j"))
-                        {
-                            if (j == 0)
-                            {
-                                i--;
-                                j = 4;
-                            }
-                            else { j--; }
-                            keyind++;
-                        }
-                        else if(key[keyind] == 'j' && contains(keymat, "i/j"))
-                        {
-                            if (j == 0)
-                            {
-                                i--;
-                                j = 4;
-                            }
-                            else { j--; }
-                            keyind++; }
-                        else
-                        {
-                            if (!contains(keymat, key[keyind].ToString())) { keymat[i, j] = key[keyind].ToString(); }
-                            else
-                            {
-                                if (j == 0) { i--; j = 4; }
-                                else { j--; }
-                            }
-                            keyind += 1;
-                        }
-                    }
-                    else
-                    {
-
-                        while (contains(keymat, currletter) && currletter.ToCharArray()[0] < 'z')
-                        {
-                            char c = currletter.ToCharArray()[0];
-                            c = (char)(c + 1);
-                            currletter = c.ToString();
-
-                        }
-                        if (currletter == "i" || currletter=="j")
-                        {
-                            currletter = "i/j";
-                            keymat[i, j] = currletter;
-                            currletter = "k";
-                        }
-                        else
-                        {
-                            keymat[i, j] = currletter;
-                        }
-                    }
-                }
-            }
+            string[,] keymat = constructKeyMatrix(key);
+            
+            
             for (int i=0; i<5; i++)
             {
                 for(int j=0; j<5; j++)
@@ -255,6 +306,77 @@ namespace SecurityLibrary
                 }
             }
             return ret;
+        }
+        public string[,] constructKeyMatrix(string key)
+        {
+            string[,] keymat = new string[5, 5];
+            int keyind = 0;
+            string currletter = "a";
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (keyind < key.Length)
+                    {
+                        if ((key[keyind] == 'i' || key[keyind] == 'j') && !contains(keymat, "i/j"))
+                        {
+                            keymat[i, j] = "i/j";
+                        }
+                        else if (key[keyind] == 'i' && contains(keymat, "i/j"))
+                        {
+                            if (j == 0)
+                            {
+                                i--;
+                                j = 4;
+                            }
+                            else { j--; }
+                            keyind++;
+                        }
+                        else if (key[keyind] == 'j' && contains(keymat, "i/j"))
+                        {
+                            if (j == 0)
+                            {
+                                i--;
+                                j = 4;
+                            }
+                            else { j--; }
+                            keyind++;
+                        }
+                        else
+                        {
+                            if (!contains(keymat, key[keyind].ToString())) { keymat[i, j] = key[keyind].ToString(); }
+                            else
+                            {
+                                if (j == 0) { i--; j = 4; }
+                                else { j--; }
+                            }
+                            keyind += 1;
+                        }
+                    }
+                    else
+                    {
+
+                        while (contains(keymat, currletter) && currletter.ToCharArray()[0] < 'z')
+                        {
+                            char c = currletter.ToCharArray()[0];
+                            c = (char)(c + 1);
+                            currletter = c.ToString();
+
+                        }
+                        if (currletter == "i" || currletter == "j")
+                        {
+                            currletter = "i/j";
+                            keymat[i, j] = currletter;
+                            currletter = "k";
+                        }
+                        else
+                        {
+                            keymat[i, j] = currletter;
+                        }
+                    }
+                }
+            }
+            return keymat;
         }
     } 
 }
